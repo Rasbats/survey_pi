bba22975f28ef12fdfb07f596e81167493619635
Rasbats <miker@members.swis.net>
2025-11-18 14:40:29 +0000

    Try old survey_pi

--- Plugin.cmake
+++ Plugin.cmake
@@ -13,19 +13,24 @@
 
 set(OCPN_TEST_REPO
     "opencpn/survey-alpha"
-    CACHE STRING "Default repository for untagged builds")
+    CACHE STRING "Default repository for untagged builds"
+)
 set(OCPN_BETA_REPO
     "opencpn/survey-beta"
-    CACHE STRING "Default repository for tagged builds matching 'beta'")
+    CACHE STRING "Default repository for tagged builds matching 'beta'"
+)
 set(OCPN_RELEASE_REPO
     "opencpn/survey-prod"
-    CACHE STRING "Default repository for tagged builds not matching 'beta'")
+    CACHE STRING "Default repository for tagged builds not matching 'beta'"
+)
+
+option(SURVEY_USE_SVG "Use SVG graphics" ON)
 
 #
 # -------  Plugin setup --------
 #
 set(PKG_NAME survey_pi)
-set(PKG_VERSION 4.2.0)
+set(PKG_VERSION 4.0.0)
 set(PKG_PRERELEASE "") # Empty, or a tag like 'beta'
 
 set(DISPLAY_NAME survey) # Dialogs, installer artifacts, ...
@@ -37,65 +42,70 @@ Survey
 ]=]
 )
 
-set(PKG_AUTHOR "Pavel Kalian, Mike Rossiter")
+set(PKG_AUTHOR "Mike Rossiter")
 set(PKG_IS_OPEN_SOURCE "yes")
-set(PKG_HOMEPAGE https://github.com/rasbats/survey_pi)
+set(PKG_HOMEPAGE https://github.com/Rasbats/survey_pi)
 set(PKG_INFO_URL https://opencpn.org/OpenCPN/plugins/survey.html)
-
-include_directories(${CMAKE_SOURCE_DIR}/include)
-add_definitions(-DocpnUSE_GL)
 
 set(SRC
     src/baro_history.cpp
+    src/baro_history.h
     src/bbox.cpp
+    src/bbox.h
+    src/circle.xpm
+    src/cross.xpm
+    src/dychart.h
     src/icons.cpp
+    src/icons.h
     src/instrument.cpp
+    src/instrument.h
     src/mygridtablebase.cpp
+    src/mygridtablebase.h
     src/ProfileWin.cpp
+    src/ProfileWin.h
     src/soundinggridtable.cpp
+    src/soundinggridtable.h
+    src/square.xpm
     src/surveygui.cpp
+    src/surveygui.h
     src/surveygui_impl.cpp
+    src/surveygui_impl.h
     src/SurveyOverlayFactory.cpp
+    src/SurveyOverlayFactory.h
     src/survey_pi.cpp
-    src/plug_utils.cpp
+    src/survey_pi.h
 )
 
-set(PKG_API_LIB api-18) # A directory in libs/ e. g., api-17 or api-16
+set(PKG_API_LIB api-17) # A directory in libs/ e. g., api-17 or api-16
 
 macro (late_init)
   # Perform initialization after the PACKAGE_NAME library, compilers and
   # ocpn::api is available.
-  # add_definitions(-D SQLITE_ENABLE_RTREE)
+  add_definitions(-D SQLITE_ENABLE_RTREE)
 
-  # Fix OpenGL deprecated warnings in Xcode
-  target_compile_definitions(${PACKAGE_NAME} PRIVATE GL_SILENCE_DEPRECATION)
-
+  if (SURVEY_USE_SVG)
+    target_compile_definitions(${PACKAGE_NAME} PUBLIC SURVEY_USE_SVG)
+  endif ()
 endmacro ()
 
 macro (add_plugin_libraries)
-# Add libraries required by this plugin
-  add_subdirectory("${CMAKE_SOURCE_DIR}/libs/std_filesystem")
-  target_link_libraries(${PACKAGE_NAME} ocpn::filesystem)
-  
-  add_subdirectory("opencpn-libs/plugin_dc")
-  target_link_libraries(${PACKAGE_NAME} ocpn::plugin-dc)
-
-  add_subdirectory("opencpn-libs/tinyxml")
+  # Add libraries required by this plugin
+  add_subdirectory("libs/tinyxml")
   target_link_libraries(${PACKAGE_NAME} ocpn::tinyxml)
 
-  add_subdirectory("opencpn-libs/wxJSON")
+  add_subdirectory("libs/wxJSON")
   target_link_libraries(${PACKAGE_NAME} ocpn::wxjson)
 
-  add_subdirectory("opencpn-libs/jsoncpp")
+  add_subdirectory("libs/plugingl")
+  target_link_libraries(${PACKAGE_NAME} ocpn::plugingl)
+
+  add_subdirectory("libs/jsoncpp")
   target_link_libraries(${PACKAGE_NAME} ocpn::jsoncpp)
 
-  add_subdirectory("opencpn-libs/nmea0183")
+  add_subdirectory("libs/nmea0183")
   target_link_libraries(${PACKAGE_NAME} ocpn::nmea0183)
 
-  #add_subdirectory("opencpn-libs/sqlite")
-  #target_link_libraries(${PACKAGE_NAME} ocpn::sqlite)
-
-  add_subdirectory("opencpn-libs/wxsvg")
-  target_link_libraries(${PACKAGE_NAME} ocpn::wxsvg)
+  add_subdirectory("libs/sqlite3_i")
+  target_link_libraries(${PACKAGE_NAME} sqlite3_i::sqlite3_i)
 
 endmacro ()
--- buildwin/win_deps.bat
+++ buildwin/win_deps.bat
@@ -7,18 +7,12 @@
 ::
 
 :: Install the pathman tool: https://github.com/therootcompany/pathman
-:: Fix PATH so it can be used in this script.
-:: The regular installation using webi is broken since long, the root
-:: cause (pun intended) is broken https setup at https://rootprojects.org.
+:: Fix PATH so it can be used in this script
 ::
-set localbin="%HomeDrive%%HomePath%\.local\bin"
-set pathman_path="buildwin\pathman.exe"
-if not "%APPVEYOR_BUILD_FOLDER%" == "" (
-    set pathman_path="%APPVEYOR_BUILD_FOLDER%\%pathman_path%"
-)
 if not exist "%HomeDrive%%HomePath%\.local\bin\pathman.exe" (
-    if not exist %localbin% mkdir %localbin%
-    copy %pathman_path%  %localbin%
+    pushd "%HomeDrive%%HomePath%"
+    curl.exe -sA "MS" https://webinstall.dev/pathman | powershell
+    popd
 )
 pathman list > nul 2>&1
 if errorlevel 1 set PATH=%PATH%;%HomeDrive%\%HomePath%\.local\bin
@@ -27,19 +21,19 @@ pathman add %HomeDrive%%HomePath%\.local\bin >nul
 :: Install choco cmake and add it's persistent user path element
 ::
 set CMAKE_HOME=C:\Program Files\CMake
-if not exist "%CMAKE_HOME%\bin\cmake.exe" choco install --no-progress -y cmake
+if not exist "%CMAKE_HOME%\bin\cmake.exe" choco install -y cmake
 pathman add "%CMAKE_HOME%\bin" > nul
 
 :: Install choco poedit and add it's persistent user path element
 ::
 set POEDIT_HOME=C:\Program Files (x86)\Poedit\Gettexttools
-if not exist "%POEDIT_HOME%" choco install --no-progress -y poedit
+if not exist "%POEDIT_HOME%" choco install -y poedit
 pathman add "%POEDIT_HOME%\bin" > nul
 
 :: Update required python stuff
 ::
 python --version > nul 2>&1 && python -m ensurepip > nul 2>&1
-if errorlevel 1 choco install --no-progress -y python
+if errorlevel 1 choco install -y python
 python --version
 python -m ensurepip
 python -m pip install --upgrade pip
@@ -50,30 +44,17 @@ python -m pip install -q cryptography
 :: Install pre-compiled wxWidgets and other DLL; add required paths.
 ::
 set SCRIPTDIR=%~dp0
-set WXWIN=%SCRIPTDIR%..\cache\wxWidgets
+set WXWIN=%SCRIPTDIR%..\cache\wxWidgets-3.1.2
 set wxWidgets_ROOT_DIR=%WXWIN%
 set wxWidgets_LIB_DIR=%WXWIN%\lib\vc_dll
 if not exist "%WXWIN%" (
-  wget --version > nul 2>&1 || choco install --no-progress -y wget
-  wget -q https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.6/wxWidgets-3.2.6-headers.7z ^
-      -O wxWidgetsHeaders.7z
-  wget -q https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.6/wxMSW-3.2.6_vc14x_ReleaseDLL.7z ^
-      -O wxWidgetsDLL.7z
-  wget -q https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.6/wxMSW-3.2.6_vc14x_Dev.7z ^
-      -O wxWidgetsDev.7z
+  wget --version > nul 2>&1 || choco install -y wget
+  wget https://download.opencpn.org/s/E2p4nLDzeqx4SdX/download ^
+      -O wxWidgets-3.1.2.7z
   7z i > nul 2>&1 || choco install -y 7zip
-  7z x -aoa wxWidgetsHeaders.7z -o%WXWIN%
-  7z x -aoa wxWidgetsDLL.7z -o%WXWIN%
-  7z x -aoa wxWidgetsDev.7z -o%WXWIN%
-  ren "%WXWIN%\lib\vc14x_dll" vc_dll
+  7z x wxWidgets-3.1.2.7z -o%WXWIN%
 )
 pathman add "%WXWIN%" > nul
 pathman add "%wxWidgets_LIB_DIR%" > nul
-
-if not exist %SCRIPTDIR%\..\cache ( mkdir %SCRIPTDIR%\..\cache )
-set "CONFIG_FILE=%SCRIPTDIR%\..\cache\wx-config.bat"
-echo set "wxWidgets_ROOT_DIR=%wxWidgets_ROOT_DIR%" > %CONFIG_FILE%
-echo set "wxWidgets_LIB_DIR=%wxWidgets_LIB_DIR%" >> %CONFIG_FILE%
-
 
 refreshenv
